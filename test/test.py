bf862a20ec8025fd254884eafa2b70e645b3211e
Divya Patil <dp8102003@gmail.com>
2025-01-20 16:29:16 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -2,14 +2,11 @@ import unittest
 from app import app
 
 class TestApp(unittest.TestCase):
-    def setUp(self):
-        self.app = app.test_client()
-        self.app.testing = True
-
     def test_home(self):
-        response = self.app.get("/")
+        tester = app.test_client(self)
+        response = tester.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode("utf-8"), "Hello, World!")
+        self.assertEqual(response.data, b"Hello, Jenkins World!")
 
 if __name__ == "__main__":
     unittest.main()
