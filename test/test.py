8b67fb6fb80af45154e77c23cf437ee197003f51
divyapatil <dp8102003@gmail.com>
2025-07-05 18:47:28 +0530

    final commit2

--- test/test.py
+++ test/test.py
@@ -6,7 +6,7 @@ class TestApp(unittest.TestCase):
         tester = app.test_client()
         response = tester.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b"Welcome to Farview Global Private Limited")
+        self.assertEqual(response.data, b"Welcome guys Farview Global Private Limited")
 
 
 if __name__ == "__main__":
